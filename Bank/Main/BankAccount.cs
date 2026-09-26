ee6bd844640fb77a0b9451be695368a6223137ab
pelme <pelme@DESKTOP-DO5DICC>
2026-09-26 17:27:19 +0700

    #2 добавлен метод GetAccountHistory

--- Bank/Main/BankAccount.cs
+++ Bank/Main/BankAccount.cs
@@ -59,6 +59,20 @@ internal class BankAccount
         _allTransactions.Add(withdrawal);
     }
 
+    public string GetAccountHistory()
+    {
+        var report = new StringBuilder();
 
+        decimal balance = 0;
+        report.AppendLine("Data\t\tAmount\tBalance\tNote");
+        foreach (var item in _allTransactions)
+        {
+            balance += item.Amount;
+            report.AppendLine($"" +
+                $"{item.Date.ToShortDateString()}\t" +
+                $"{item.Amount}\t{balance}\t{item.Note}");
+        }
+        return report.ToString();
+    }
 
 }
